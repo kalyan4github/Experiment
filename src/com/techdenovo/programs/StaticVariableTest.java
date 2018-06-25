8ceeca8ec9159fce70e26082e4e6e7b7e1ab24ba
kalyan mazumder <kalyaninform@gmail.com>
2018-06-25 16:57:36 +0530

    initial first save

---
+++ src/com/techdenovo/programs/StaticVariableTest.java
@@ -0,0 +1,17 @@
+package com.techdenovo.programs;
+
+public class StaticVariableTest {
+	
+	void function1() {
+		
+	}
+	void function1(Static int x) {
+		
+	}
+
+	public static void main(String[] args) {
+		StaticVariableTest svt = new StaticVariableTest();
+
+	}
+
+}
